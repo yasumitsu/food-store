f62f8a768b85de7af682afab3df80bbcb2022409
Kenji <kenjiyasumitsu@gmail.com>
2021-09-30 19:00:12 +0900

    add logic to additemscart

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import styles from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = ({ onClick }) => {
+	const cartCtx = useContext(CartContext);
+
+	const cartItemsQty = cartCtx.items.reduce((cur, item) => cur + item.amount, 0);
+
 	return (
 		<button className={styles.button} onClick={onClick}>
 			<span className={styles.icon}>
 				<CartIcon />
 			</span>
 			<span>Your Cart</span>
-			<span className={styles.badge}>3</span>
+			<span className={styles.badge}>{cartItemsQty}</span>
 		</button>
 	);
 };
--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,18 +1,33 @@
+import { useContext } from 'react';
 import styles from './MealItem.module.css';
+
+import CartContext from '../../../store/cart-context';
 import MealItemForm from './MealItemForm';
 
-const MealItem = ({ name, description, price }) => {
-	price = `$${price.toFixed(2)}`;
+const MealItem = ({ name, description, price, id }) => {
+	const cartCtx = useContext(CartContext);
+
+	const priceFixed = `$${price.toFixed(2)}`;
+
+	const addToCartHandler = (amount) => {
+		cartCtx.addItem({
+			id,
+			name,
+			amount,
+			price
+		});
+		console.log(cartCtx.amount);
+	};
 
 	return (
 		<li className={styles.meal}>
 			<div>
 				<h3>{name}</h3>
 				<div className={styles.description}>{description}</div>
-				<div className={styles.price}>{price}</div>
+				<div className={styles.price}>{priceFixed}</div>
 			</div>
 			<div>
-				<MealItemForm />
+				<MealItemForm onAddToCart={addToCartHandler} />
 			</div>
 		</li>
 	);
