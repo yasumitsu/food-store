b3bf480117c7d1f44dd19916b425ba0fb8342f05
Kenji <kenjiyasumitsu@gmail.com>
2021-10-04 22:42:32 +0900

    fetching data from firebase

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,38 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 
 import styles from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-	{
-		id: 'm1',
-		name: 'Sushi',
-		description: 'Finest fish and veggies',
-		price: 22.99
-	},
-	{
-		id: 'm2',
-		name: 'Schnitzel',
-		description: 'A german specialty!',
-		price: 16.5
-	},
-	{
-		id: 'm3',
-		name: 'Barbecue Burger',
-		description: 'American, raw, meaty',
-		price: 12.99
-	},
-	{
-		id: 'm4',
-		name: 'Green Bowl',
-		description: 'Healthy...and green...',
-		price: 18.99
-	}
-];
-
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map((meal) => (
+	const [ getMeals, setMeals ] = useState([]);
+	const [ getLoading, setLoading ] = useState(true);
+	const [ getDbError, setDbError ] = useState();
+
+	useEffect(() => {
+		const fetchMeals = async () => {
+			const response = await fetch(
+				'https://true-harmony-355-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json'
+			);
+
+			if (!response.ok) throw new Error('Something went wrong.');
+
+			const responseData = await response.json();
+
+			const loadedMeals = [];
+
+			for (const key in responseData) {
+				loadedMeals.push({
+					id: key,
+					name: responseData[key].name,
+					description: responseData[key].description,
+					price: responseData[key].price
+				});
+			}
+
+			setMeals(loadedMeals);
+			setLoading(false);
+		};
+
+		fetchMeals().catch((error) => {
+			setLoading(false);
+			setDbError(error.message);
+		});
+	}, []);
+
+	if (getLoading) {
+		return (
+			<section className={styles.mealsLoading}>
+				<p>Loading...</p>
+			</section>
+		);
+	}
+
+	if (getDbError) {
+		return (
+			<section className={styles.mealsError}>
+				<p>{getDbError}</p>
+			</section>
+		);
+	}
+
+	const mealsList = getMeals.map((meal) => (
 		<MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />
 	));
 
