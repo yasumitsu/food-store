778dabb2302bbfe1f6c32661bce22ae50cde9460
Kenji <kenjiyasumitsu@gmail.com>
2021-10-05 21:38:39 +0900

    add clear cart action

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { Fragment, useContext, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import styles from './Cart.module.css';
@@ -7,6 +7,8 @@ import Checkout from './Checkout';
 
 const Cart = ({ onClose }) => {
 	const [ getCheckout, setCheckout ] = useState(false);
+	const [ getSubmitting, setSubmitting ] = useState(false);
+	const [ getSubmitted, setSubmitted ] = useState(false);
 	const cartCtx = useContext(CartContext);
 
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -18,21 +20,25 @@ const Cart = ({ onClose }) => {
 	};
 
 	const cartItemAddHandler = (item) => {
-		cartCtx.addItem({ ...item, amount: 1 });
+		cartCtx.addItem(item);
 	};
 
 	const orderHandler = () => {
 		setCheckout(true);
 	};
 
-	const submitOrderHandler = (userData) => {
-		fetch('https://true-harmony-355-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json', {
+	const submitOrderHandler = async (userData) => {
+		setSubmitting(true);
+		await fetch('https://true-harmony-355-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json', {
 			method: 'POST',
 			body: JSON.stringify({
 				user: userData,
 				orderedItems: cartCtx.items
 			})
 		});
+		setSubmitting(false);
+		setSubmitted(true);
+		cartCtx.clearCart();
 	};
 
 	const cartItems = (
@@ -63,8 +69,8 @@ const Cart = ({ onClose }) => {
 		</div>
 	);
 
-	return (
-		<Modal onClose={onClose}>
+	const cartModalContent = (
+		<Fragment>
 			{cartItems}
 			<div className={styles.total}>
 				<span>Total Amount</span>
@@ -72,6 +78,26 @@ const Cart = ({ onClose }) => {
 			</div>
 			{getCheckout && <Checkout onCancel={onClose} onConfirm={submitOrderHandler} />}
 			{!getCheckout && modalActions}
+		</Fragment>
+	);
+
+	const submittingContent = <p>Sending data to server...</p>;
+	const submittedContent = (
+		<Fragment>
+			<p>Order success!</p>
+			<div className={styles.actions}>
+				<button className={styles.button} onClick={onClose}>
+					Close
+				</button>
+			</div>
+		</Fragment>
+	);
+
+	return (
+		<Modal onClose={onClose}>
+			{!getSubmitting && !getSubmitted && cartModalContent}
+			{getSubmitting && submittingContent}
+			{!getSubmitting && getSubmitted && submittedContent}
 		</Modal>
 	);
 };
--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,4 +1,5 @@
 import { useReducer } from 'react';
+
 import CartContext from './cart-context';
 
 const defaultCartState = {
@@ -12,7 +13,6 @@ const cartReducer = (state, action) => {
 
 		const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
 		const existingCartItem = state.items[existingCartItemIndex];
-
 		let updatedItems;
 
 		if (existingCartItem) {
@@ -31,23 +31,27 @@ const cartReducer = (state, action) => {
 			totalAmount: updatedTotalAmount
 		};
 	}
-
 	if (action.type === 'REMOVE') {
 		const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
-		const existingCartItem = state.items[existingCartItemIndex];
-
-		const updatedTotalAmount = state.totalAmount - existingCartItem.price;
-
+		const existingItem = state.items[existingCartItemIndex];
+		const updatedTotalAmount = state.totalAmount - existingItem.price;
 		let updatedItems;
-		if (existingCartItem.amount === 1) {
+		if (existingItem.amount === 1) {
 			updatedItems = state.items.filter((item) => item.id !== action.id);
 		} else {
-			const updatedItem = { ...existingCartItem, amount: existingCartItem.amount - 1 };
+			const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
 			updatedItems = [ ...state.items ];
 			updatedItems[existingCartItemIndex] = updatedItem;
 		}
 
-		return { items: updatedItems, totalAmount: updatedTotalAmount };
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount
+		};
+	}
+
+	if (action.type === 'CLEAR') {
+		return defaultCartState;
 	}
 
 	return defaultCartState;
@@ -64,11 +68,16 @@ const CartProvider = (props) => {
 		dispatchCartAction({ type: 'REMOVE', id: id });
 	};
 
+	const clearCartHandler = () => {
+		dispatchCartAction({ type: 'CLEAR' });
+	};
+
 	const cartContext = {
 		items: cartState.items,
 		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
-		removeItem: removeItemFromCartHandler
+		removeItem: removeItemFromCartHandler,
+		clearCart: clearCartHandler
 	};
 
 	return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
